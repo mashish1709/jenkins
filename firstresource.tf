6c3879806780cb028a8a656b88df696fdb6efc3b
mashish1709 <113689083+mashish1709@users.noreply.github.com>
2022-11-01 10:29:56 +0530

    Update firstresource.tf

--- firstresource.tf
+++ firstresource.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket = "my-prod-terra-tfstate-bucket"
+    region = "us-east-1"
+    key="terraform.tfstate"
+    dynamodb_table = "my-testingtfstate-table"
+  }
+}
+
 resource "aws_instance" "web" {
   ami           = "ami-09d3b3274b6c5d4aa"
   instance_type = "t2.micro"
